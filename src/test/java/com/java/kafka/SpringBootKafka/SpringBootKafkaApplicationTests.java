5b7c02602413ba7b82d79ca99b70948518438db3
Sakil <sakilahammadlaskar786@gmail.com>
2024-06-07 01:28:12 +0530

    Adding KAfka commit

---
+++ src/test/java/com/java/kafka/SpringBootKafka/SpringBootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.kafka.SpringBootKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
